4abcec6b651f7519bd9603fbe514e57cccb309f2
arielzissu <arielzissu98@gmail.com>
2025-05-02 23:06:34 +0300

    fixed cors

--- src/server.js
+++ src/server.js
@@ -8,7 +8,14 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(cors());
+app.use(
+  cors({
+    origin: "*", // or "https://wedding-memory-client.vercel.app"
+    methods: ["GET", "POST", "DELETE"],
+    credentials: false,
+  })
+);
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
